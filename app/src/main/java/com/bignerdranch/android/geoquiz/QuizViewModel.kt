0a0ca70c605d573ef4a8cc314aa82af1bf98ce62
Adrian Q Montgomery <maia.business.solutions.llc@gmail.com>
2023-04-04 21:35:41 -0400

    Use SavedStateHandle

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
@@ -1,10 +1,13 @@
 package com.bignerdranch.android.geoquiz
 
+import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
 private const val TAG = "QuizViewModel"
+const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
 
-class QuizViewModel : ViewModel() {
+class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -15,7 +18,17 @@ class QuizViewModel : ViewModel() {
         Question(R.string.question_asia, true)
     )
 
-    private var currentIndex: Int = 0
+    private var currentIndex: Int
+        get() {
+            val retVal = savedStateHandle.get<Int>(CURRENT_INDEX_KEY) ?: 0
+            Log.d(TAG, "Retrieved =  $retVal")
+            return retVal
+        }
+
+        set(value) {
+            savedStateHandle.set(CURRENT_INDEX_KEY, value)
+            Log.d(TAG, "Set to $value")
+        }
 
     val currentQuestionAnswer: Boolean
         get() = questionBank[currentIndex].answer
